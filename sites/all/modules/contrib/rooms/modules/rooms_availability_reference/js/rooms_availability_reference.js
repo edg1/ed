327460db34f4938d188acd00eb1150c657603917
root <root@ed.localdomain>
2015-01-23 13:15:08 +0100

    rooms werkt

--- sites/all/modules/contrib/rooms/modules/rooms_availability_reference/js/rooms_availability_reference.js
+++ sites/all/modules/contrib/rooms/modules/rooms_availability_reference/js/rooms_availability_reference.js
@@ -3,26 +3,24 @@
 
 Drupal.behaviors.rooms_availability_reference = {
   attach: function(context) {
-    var minDate =new Date();
-    var i = 0;
+    var today = moment();
 
     $('.cal').once('cal', function() {
-      var j = i;
       var lastSource;
+      var cal_id = $(this).siblings('.availability-title').attr('id');
 
       $(this).fullCalendar({
         ignoreTimezone:false,
         editable:false,
         dayNamesShort:[Drupal.t("Sun"), Drupal.t("Mon"), Drupal.t("Tue"), Drupal.t("Wed"), Drupal.t("Thu"), Drupal.t("Fri"), Drupal.t("Sat")],
         monthNames:[Drupal.t("January"), Drupal.t("February"), Drupal.t("March"), Drupal.t("April"), Drupal.t("May"), Drupal.t("June"), Drupal.t("July"), Drupal.t("August"), Drupal.t("September"), Drupal.t("October"), Drupal.t("November"), Drupal.t("December")],
-        month:minDate.getMonth(),
-        year:minDate.getFullYear(),
+        defaultDate: today,
         header:{
           left: 'today',
           center: 'title',
           right: 'prev, next'
         },
-        viewDisplay: function(view) {
+        viewRender: function(view) {
           if (view.name == 'month') {
             for (var url in lastSource) {
               view.calendar.removeEventSource(lastSource[url]);
@@ -30,9 +28,9 @@ Drupal.behaviors.rooms_availability_reference = {
             view.calendar.refetchEvents();
 
             lastSource = [];
-            for (var index = 0; index < Drupal.settings.roomsAvailabilityRef.length; index++) {
-              url = '?q=rooms/units/unit/' + Drupal.settings.roomsAvailabilityRef[index].unitID + '/availability/json/'
-                + view.start.getFullYear() + '/' + (view.start.getMonth() + 1);
+            for (var index = 0; index < Drupal.settings.roomsAvailabilityRef[cal_id].unitID.length; index++) {
+              url = '?q=rooms/units/unit/' + Drupal.settings.roomsAvailabilityRef[cal_id].unitID[index] + '/availability/json/'
+                + view.intervalStart.get('year') + '/' + (view.intervalStart.get('month') + 1);
 
                 view.calendar.addEventSource(url);
 
@@ -40,10 +38,13 @@ Drupal.behaviors.rooms_availability_reference = {
             }
 
           }
+        },
+        //Remove Time from events
+        eventRender: function(event, el) {
+          el.find('.fc-time').remove();
         }
       });
 
-      i++;
     });
 
 
